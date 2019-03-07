cf2461981bd0002c17cda6c534947b602e98e6de
Matt Travi <programmer@travi.org>
2019-03-06 23:35:22 -0600

    feat(private-badge): returned version with slug for token

--- src/badge-scaffolder.js
+++ src/badge-scaffolder.js
@@ -1,7 +1,8 @@
-export default function scaffoldBadgeDetails(vcs) {
+export default function scaffoldBadgeDetails(vcs, visibility) {
   return {
     text: 'CircleCI',
     link: `https://circleci.com/gh/${vcs.owner}/${vcs.name}`,
-    img: `https://img.shields.io/circleci/project/github/${vcs.owner}/${vcs.name}/master.svg`
+    img: `https://img.shields.io/circleci/${'Private' === visibility ? 'token/<token here>/' : ''}` +
+      `project/github/${vcs.owner}/${vcs.name}/master.svg`
   };
 }
--- test/unit/badge-scaffolder-test.js
+++ test/unit/badge-scaffolder-test.js
@@ -16,4 +16,19 @@ suite('badge scaffolder', () => {
       }
     );
   });
+
+  test('that the private version is returned for `Private` projects', () => {
+    const vcsHostOwner = any.word();
+    const vcsHostRepositoryName = any.word();
+
+    assert.deepEqual(
+      scaffoldBadge({owner: vcsHostOwner, name: vcsHostRepositoryName}, 'Private'),
+      {
+        text: 'CircleCI',
+        link: `https://circleci.com/gh/${vcsHostOwner}/${vcsHostRepositoryName}`,
+        img: `https://img.shields.io/circleci/token/<token here>/project/github/${vcsHostOwner}/` +
+          `${vcsHostRepositoryName}/master.svg`
+      }
+    );
+  });
 });
